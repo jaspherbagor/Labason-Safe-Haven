9a27de518e4b6460f749949de663a89fe5c8ccef
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-03-15 16:48:53 +0800

    added styles to the hero heading

--- labason-safe-haven/resources/views/home.blade.php
+++ labason-safe-haven/resources/views/home.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.client.layout')
 
 @section('content')
-<h1 class="fw-bolder">This is content for the home page</h1>
+<section class="hero-section container-fluid vh-100 bg-success d-flex align-items-center justify-content-center">
+    <div class="container hero-text text-center">
+        <h1 class="fw-medium display-3 hero-heading heading">FIND YOUR NEXT HOME</h1>
+        <p class="fs-5 sub-heading">A place where you feel safe and at peace</p>
+    </div>
+    
+</section>
 @endsection
